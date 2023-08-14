eefca6396eb7b584f5357b79bc7823321b1e517a
sujeong6199 <tnwjd6199@naver.com>
2023-08-14 10:14:35 +0900

    Ing:home
    메인홈화면 분리 작업 중입니다.

---
+++ src/main/webapp/view_customer/home/header/header.js
@@ -0,0 +1,22 @@
+/* 클릭 시 submenu 노출*/
+document.addEventListener("DOMContentLoaded", function() {
+    var myPageLink = document.querySelector(".my-page");
+    var submenu = myPageLink.querySelector(".submenu");
+
+    myPageLink.addEventListener("mouseenter", function() {
+        submenu.style.display = "block";
+    });
+
+    myPageLink.addEventListener("mouseleave", function() {
+        submenu.style.display = "none";
+    });
+
+    submenu.addEventListener("mouseenter", function() {
+        submenu.style.display = "block";
+    });
+
+    submenu.addEventListener("mouseleave", function() {
+        submenu.style.display = "none";
+    });
+});
+
