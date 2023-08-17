290ef24835c11155f0bbf071ec61b41613f3abb3
elane <chagaram0610@gmail.com>
2023-08-17 16:22:12 +0900

    Feat : customer-info
    고객 정보 수정, 판매자 정보 수정 완성

--- src/main/java/mysql/db_dao.java
+++ src/main/java/mysql/db_dao.java
@@ -351,7 +351,7 @@ public class db_dao {
     public HashMap<String, String> getSellerInfo(String currentUser) {
         Connection conn = db_util.getConnection();
         int currentUID = getUidByEmail(currentUser);
-        System.out.println("currentUId = " + currentUID);
+        //System.out.println("currentUId = " + currentUID);
         String SQL = "select email, name, pw, phone, company, address, zipcode from user where role = 1 AND uid = ?;";
 
         try {
@@ -467,8 +467,8 @@ public class db_dao {
         try {
             // 데이터베이스 연결 및 쿼리 실행
             PreparedStatement pstmt = conn.prepareStatement(SQL);
-
-            pstmt.setInt(1, 1); // 판매자 ID를 설정하세요
+            pstmt.setInt(1, currentUID); // 고객 UID
+            System.out.println(currentUID);
             rs = pstmt.executeQuery();
 
             if (rs.next()) {
@@ -487,6 +487,7 @@ public class db_dao {
                 customerInfo.put("extraAddress", addressParts[3]); // 네 번째 부분 저장
                 customerInfo.put("postCode", rs.getString("zipcode")); // 우편 번호
             }
+            return customerInfo;
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -499,7 +500,53 @@ public class db_dao {
             }
         }
 
-        return customerInfo;
+       return null;
+    }
+
+    // 고객 정보 수정 디비에 적용하는 함수
+    public int customerUpdateInfo(String currentUser, String modifyName, String modifyPw, String modifyPhone, String modifyBirthday, String modifySex, String postCode, String fullAddress) {
+        Connection conn = db_util.getConnection();
+        int currentUID = getUidByEmail(currentUser);
+        // 권한에 맞춰서 다르게 작성 - role(0 고객, 1 판매자)
+        //(판매자) UPDATE user SET name = ?, pw = ?, phone = ?, company = ?, address = ? WHERE role = 1 AND UID = ?;
+        //(고객 ) UPDATE user SET name = ?, pw =?, phone =?, gender = ?, birthday =?, address =? WHERE role = 0 AND UID = ?;
+        //판매자
+        String SQL = "UPDATE user SET name = ?, pw =?, phone =?, gender = ?, birthday =?, address =?, zipcode = ? WHERE role = 0 AND UID = ?;";
+
+        try {
+            // 실행 가능 상태의 sql문으로 만듦.
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+
+            // 쿼리문의 ?안에 각각의 데이터를 넣어준다.
+            pstmt.setString(1, modifyName);
+            pstmt.setString(2, modifyPw);
+            pstmt.setString(3, modifyPhone);
+            pstmt.setString(4, modifySex);
+            pstmt.setString(5, modifyBirthday);
+            pstmt.setString(6, fullAddress);
+            pstmt.setString(7, postCode);
+            pstmt.setInt(8, currentUID);
+
+            // 명령어를 수행한 결과
+            // 1_execute -> 테이블 생성, 수정, 삭제 등 데이터베이스 관리 명령어 사용(table)
+            // 2_executeUpdate -> 레코드 삽입, 수정, 삭제 등 데이터 조작 명령어 사용(CRUD)
+            // 3_executeQuery -> 레코드 조회, 테이블 조회 등 조회 명령어 사용(Select)
+
+            return pstmt.executeUpdate(); // 1은 데이터 삽입 성공, 0은 데이터 삽입 실패
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if(conn != null&& !conn.isClosed())
+                    conn.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return -1;
     }
 }
 
