723986913c4e4f66a9c88f176e48485789e39b2e
sujeong6199 <tnwjd6199@naver.com>
2023-08-21 00:38:26 +0900

    Ing : basket.jsp
     basket.jsp 출력완료
    삭제 , 수정 해야 함

--- src/main/java/mysql/ShoppingCartDTO.java
+++ src/main/java/mysql/ShoppingCartDTO.java
@@ -7,13 +7,22 @@ public class ShoppingCartDTO {
     private int uid; // 고객 코드 (FK)
     private int pid; // 제품 코드 (FK)
     private int quantitiy; // 고객이 담은 제품 수량
+    private String product_name; // 제품이름
+    private int cost; // 가격
+    private String pimage; // 제품 이미지
 
     /*생성자---------------------------------------------------*/
 
     // 기본 생성자
     public ShoppingCartDTO() {
     }
-
+    //고객 장바구니 출력을 위한 (이미지 / 제품명 / 주문 수량 / 금액)을 담은 생성자
+    public ShoppingCartDTO(String pimage, String product_name, int quantitiy, int cost) {
+        this.pimage = pimage;
+        this.product_name = product_name;
+        this.quantitiy = quantitiy;
+        this.cost = cost;
+    }
 
     /*getter---------------------------------------------------*/
     public int getSid() {
@@ -31,4 +40,12 @@ public class ShoppingCartDTO {
     public int getQuantitiy() {
         return quantitiy;
     }
+
+    public String getProduct_name() {return product_name;}
+
+    public int getCost() {return cost;}
+
+    public String getPimage() {return pimage;}
+
+    public int getTotalCost() {return quantitiy * cost;}
 }
